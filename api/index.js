279dc6f1756c24ed5527a34624bd209269875706
Tejas Nayak <arpithanayak900@gmail.com>
2024-12-10 21:15:05 +0530

    update

---
+++ api/index.js
@@ -0,0 +1,18 @@
+let express = require("express");
+let app = express();
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+
+app.route("/")
+.get((req, res) => {
+    const twiml = new VoiceResponse();
+
+    twiml.say('Hello from your pals at Twilio! Have fun.');
+    console.log("hi");
+
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
+});
+
+app.listen(10000, () => {
+    console.log('TwiML server running at http://127.0.0.1:1337/');
+});
